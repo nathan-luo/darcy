b6db8d0401217b0ba5f6d6100ea4f5f4d3efe734
Henry Routson <hroutson@student.unimelb.edu.au>
2025-04-01 20:36:36 +1100

    init: add import and class

--- src/llmgine/notion-api-functions.py/notion-api-functions.py
+++ src/llmgine/notion-api-functions.py/notion-api-functions.py
@@ -1,31 +1,53 @@
-type person_id = str
-type project_id = str
-type task_id = str
-type progress = str
-type date = str
+import os
+
+import notion_client
+
+type person_id_type = str
+type project_id_type = str
+type task_id_type = str
+type progress_type = str
+type date_type = str
 
 
-def create_filter_object_(
-    *properties,
-):  # for each database # Figure out the Filter Object Syntax
-    pass
+class NotionTaskAPI:
+    def __init__(self):
+        self.client = notion_client.Client(auth=os.getenv("NOTION_API_KEY"))
 
+    def create_filter_object_(self):
+        # for each database # Figure out the Filter Object Syntax
+        pass
 
-def get_tasks(person_id, project_id):  # Function to read tasks from projects database
-    pass
+    def get_tasks(self, person_id: person_id_type, project_id: project_id_type):
+        # Function to read tasks from projects database
+        print(person_id, project_id)
+        pass
 
+    def update_task(
+        self,
+        task_id: task_id_type,
+        progress: progress_type,
+        date: date_type,
+        person: person_id_type,
+    ):
+        # Function to update tasks
+        print(task_id, progress, date, person)
+        pass
 
-def update_task(task_id, progress, date, person):  # Function to update tasks
-    pass
+    def todo(self):
+        # Be able to update a field with a specific user
+        pass
 
+    def get_active_projects(self):
+        # -> Parsed list of project ids and project names # Function to query active project
+        pass
 
-def todo():  # Be able to update a field with a specific user
-    pass
-
-
-def get_active_projects():  # -> Parsed list of project ids and project names # Function to query active project
-    pass
-
-
-def create_task(task_name, due_date, person_id, project_id):  # Function to create tasks
-    pass
+    def create_task(
+        self,
+        task_name: str,
+        due_date: date_type,
+        person_id: person_id_type,
+        project_id: project_id_type,
+    ):
+        # Function to create tasks
+        print(task_name, due_date, person_id, project_id)
+        pass
