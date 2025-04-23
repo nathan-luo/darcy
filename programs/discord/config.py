220144888301616e4138fd28f25c9d7c7f587be2
Henry Routson <hroutson@student.unimelb.edu.au>
2025-04-23 16:51:22 +1000

    init

--- programs/discord/config.py
+++ programs/discord/config.py
@@ -17,10 +17,51 @@ import os
 import dotenv
 import sys
 
+
 # Add the parent directory to the path so we can import from sibling directories
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../.."))
 
 from llmgine.bootstrap import ApplicationConfig
+
+
+
+
+
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+def validate_env_keys() :
+
+  assert(os.getenv("NOT A KEY") == None)
+
+  # TODO move this somewhere else
+  required_keys = [
+    "OPENAI_API_KEY", 
+    "OPENROUTER_API_KEY", 
+    "NOTION_TOKEN", 
+    "NOTION_API_KEY", 
+    "NOTION_TESTING_DATABASE_ID", 
+    "NOTION_PRODUCTION_DATABASE_ID_TASKS", 
+    "NOTION_PRODUCTION_DATABASE_ID_PROJECTS", 
+    "TEST_SERVER_ID", 
+    "DARCY_KEY", 
+    "DARYL_KEY",
+  ]
+
+
+  for key in required_keys :
+    val = os.getenv(key)
+    if (val == None) :
+      raise Exception(f"{key} is not in env keys")
+
+
+
+
+
+
+
 
 @dataclass
 class DiscordBotConfig(ApplicationConfig):
@@ -42,6 +83,7 @@ class DiscordBotConfig(ApplicationConfig):
     @classmethod
     def load_from_env(cls) -> 'DiscordBotConfig':
         """Load configuration from environment variables."""
+        validate_env_keys()
         dotenv.load_dotenv(override=True)
         config = cls()
         config.bot_key = os.getenv("BOT_KEY")
---
+++ programs/env_validate.py
@@ -0,0 +1,3 @@
+
+
+# This is a file to validate environment variables
